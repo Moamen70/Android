64f9762e1d5bc122ac921a00a1ec48315fc57fb4
Moamen Mohamed <moamenmohamed664@gmail.com>
2023-02-08 18:40:06 +0200

    First Application

---
+++ DiceApp/app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.diceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
