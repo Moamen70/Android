732cf7f50ca000fe565f7fba9da260db185b256a
Moamen Mohamed <moamenmohamed664@gmail.com>
2023-02-18 23:48:37 +0200

    Firebase authentication project

---
+++ Firebase/app/src/main/java/com/example/firebaseproject/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.firebaseproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.firebaseproject.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.*
+import kotlinx.coroutines.tasks.await
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnReg.setOnClickListener {
+            registerUser()
+        }
+
+        binding.btnLog.setOnClickListener {
+            logInUser()
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.signOut()
+    }
+
+    private fun registerUser() {
+        val email = binding.etRegEmail.text.toString()
+        val pass = binding.etRegPass.text.toString()
+        if (email.isNotEmpty() && pass.isNotEmpty()){
+           CoroutineScope(Dispatchers.IO).launch {
+               try {
+                    auth.createUserWithEmailAndPassword(email,pass).await()
+                   withContext(Dispatchers.Main){
+                       checkedLoggedInState()
+                   }
+               }
+               catch (e:Exception){
+                   withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity,e.message.toString(),Toast.LENGTH_SHORT).show()
+                   }
+               }
+           }
+        }
+
+    }
+
+    private fun logInUser() {
+        val email = binding.etLogEmail.text.toString()
+        val pass = binding.etLogPass.text.toString()
+        if (email.isNotEmpty() && pass.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(email,pass).await()
+                    withContext(Dispatchers.Main){
+                        checkedLoggedInState()
+                    }
+                }
+                catch (e:Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@MainActivity,e.message.toString(),Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun checkedLoggedInState() {
+        if (auth.currentUser == null){
+            binding.tvResult.text ="You are not logged in"
+        }
+        else binding.tvResult.text ="Logged in"
+    }
+}
